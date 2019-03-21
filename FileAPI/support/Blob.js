00d81eda58e911bbf114f3ed22254cff92ff66c1
Jarryd <jarrydg@chromium.org>
2019-03-21 10:37:47 -0700

    FileAPI: Add web platform tests for new Blob APIs.
    Change-Id: Ie12aa7cd3d79fa579cb68345b8d201342ed99ae5

--- FileAPI/support/Blob.js
+++ FileAPI/support/Blob.js
@@ -47,3 +47,51 @@ function test_blob_binary(fn, expectations) {
     fr.readAsArrayBuffer(blob);
   });
 }
+
+function test_blob_readable_stream(fn, expectations) {
+  const { expected, desc } = expectations;
+  promise_test(async function () {
+    const stream = fn();
+    assert_true(stream instanceof ReadableStream);
+
+    assert_true('getReader' in stream);
+    const reader = stream.getReader();
+
+    assert_true('read' in reader);
+    let read_value = await reader.read();
+
+    let combined_chunks = ""
+    while (!read_value.done) {
+      let blob_string = new TextDecoder("utf-8").decode(read_value.value);
+      combined_chunks += blob_string;
+      read_value = await reader.read();
+    }
+
+    assert_equals(combined_chunks, expected);
+  }, desc);
+}
+
+function test_blob_text(fn, expectations) {
+  const { expected, desc } = expectations;
+  promise_test(async function () {
+    const text = await fn();
+    assert_equals(typeof text, String);
+
+    assert_equals(text, expected);
+  }, desc);
+}
+
+function test_blob_array_buffer(fn, expectations) {
+  const { expected, desc } = expectations;
+
+  promise_test(async () => {
+    const array_buffer = await fn();
+    assert_true(array_buffer instanceof ArrayBuffer);
+
+    assert_array_equals(array_buffer_to_string(array_buffer), expected);
+  })
+}
+
+function array_buffer_to_string(buffer) {
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
